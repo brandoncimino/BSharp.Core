9d6071f642acd8798c43d9bbb942b901ac454097
Brandon Cimino <brandon.cimino@gmail.com>
2024-01-21 22:21:40 -0600

    Added `RandomExtensions`

---
+++ BSharp.Core.Tests/RandomExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.CompilerServices;
+using NUnit.Framework;
+
+namespace BSharp.Core.Tests;
+
+public class RandomExtensionsTests
+{
+    private static Random CreateRandom([CallerMemberName] string? caller = null)
+    {
+        ArgumentNullException.ThrowIfNull(caller);
+        return new Random(caller.Sum(static c => c));
+    }
+    
+    [Test]
+    public void NextElement_IList()
+    {
+        var choices = new[] { 1, 2, 3 };
+        var viaExtension = CreateRandom().NextElement(choices);
+        var viaVanilla = choices[CreateRandom().Next(choices.Length)];
+
+        Assert.That(viaExtension, Is.EqualTo(viaVanilla));
+    }
+
+    [Test]
+    public void NextElement_Span()
+    {
+        Span<int> choices = new[] { 1, 2, 3 };
+        var viaExtension = CreateRandom().NextElement(choices);
+        var viaVanilla = choices[CreateRandom().Next(choices.Length)];
+
+        Assert.That(viaExtension, Is.EqualTo(viaVanilla));
+    }
+
+    [Test]
+    public void NextElement_RoSpan()
+    {
+        ReadOnlySpan<int> choices = new[] { 1, 2, 3 };
+        var viaExtension = CreateRandom().NextElement(choices);
+        var viaVanilla = choices[CreateRandom().Next(choices.Length)];
+        
+        Assert.That(viaExtension, Is.EqualTo(viaVanilla));
+    }
+}
---
+++ BSharp.Core/RandomExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace BSharp.Core;
+
+public static class RandomExtensions
+{
+    private static readonly Random SharedRandom =
+#if NET7_0_OR_GREATER
+        Random.Shared;
+#else
+    new Random();
+#endif
+    
+    /// <returns>this <see cref="Random"/>, if it is non-<c>null</c>; otherwise, <see cref="Random.Shared"/></returns>
+    [Pure]
+    public static Random OrShared(this Random? random) => random ?? SharedRandom;
+    
+    /// <param name="generator">a <see cref="Random"/> instance <i>(defaults to <see cref="Random.Shared"/>)</i></param>
+    /// <param name="stuff">some stuff to choose from</param>
+    /// <typeparam name="T">the type of <paramref name="stuff"/></typeparam>
+    /// <returns>a random <see cref="IList{T}.this"/> element from <paramref name="stuff"/></returns>
+    [Pure]
+    public static T NextElement<T>(this Random generator, IList<T> stuff) => stuff[generator.Next(stuff.Count)];
+
+    /// <inheritdoc cref="NextElement{T}(System.Random,System.Collections.Generic.IList{T})"/>
+    [Pure]
+    public static T NextElement<T>(this Random generator, ReadOnlySpan<T> stuff) =>
+        stuff[generator.Next(stuff.Length)];
+
+    /// <inheritdoc cref="NextElement{T}(System.Random,System.Collections.Generic.IList{T})"/>
+    [Pure]
+    public static T NextElement<T>(this Random generator, Span<T> stuff) => stuff[generator.Next(stuff.Length)];
+    
+    /// <param name="stuff">some stuff to choose from</param>
+    /// <param name="generator">a <see cref="Random"/> instance <i>(defaults to <see cref="Random.Shared"/>)</i></param>
+    /// <typeparam name="T">the type of <paramref name="stuff"/></typeparam>
+    /// <returns>a random <see cref="IList{T}.this"/> element from <paramref name="stuff"/></returns>
+    [Pure]
+    public static T RandomElement<T>(this IList<T> stuff, Random? generator = null) =>
+        generator.OrShared().NextElement(stuff);
+
+    /// <inheritdoc cref="RandomElement{T}(System.Collections.Generic.IList{T},System.Random?)"/>
+    /// <returns>a random <see cref="ReadOnlySpan{T}.this"/> element from <paramref name="stuff"/></returns>
+    [Pure]
+    public static T RandomElement<T>(this ReadOnlySpan<T> stuff, Random? generator = null) =>
+        generator.OrShared().NextElement(stuff);
+
+    /// <inheritdoc cref="RandomElement{T}(System.Collections.Generic.IList{T},System.Random?)"/>
+    /// <returns>a random <see cref="Span{T}.this"/> element from <paramref name="stuff"/></returns>
+    [Pure]
+    public static T RandomElement<T>(this Span<T> stuff, Random? generator = null) =>
+        generator.OrShared().NextElement(stuff);
+}
