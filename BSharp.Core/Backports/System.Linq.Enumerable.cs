25dae7dd65d4e6ae3a81f79476f77fd1cac952bc
Brandon Cimino <brandon.cimino@gmail.com>
2024-02-02 11:24:48 -0600

    Backported `Enumerable.TryGetNonEnumeratedCount()`

---
+++ BSharp.Core/Backports/System.Linq.Enumerable.cs
@@ -0,0 +1,19 @@
+﻿#if !NET7_0_OR_GREATER
+// ReSharper disable CheckNamespace
+namespace System.Linq;
+
+internal static partial class Enumerable
+{
+    public static bool TryGetNonEnumeratedCount<T>(this IEnumerable<T> source, out int count)
+    {
+        if (source is IList<T> list)
+        {
+            count = list.Count;
+            return true;
+        }
+
+        count = default;
+        return false;
+    }
+}
+#endif
