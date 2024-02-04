97061993bc9c514396d334dfaf810217bf221553
Brandon Cimino <brandon.cimino@gmail.com>
2024-02-03 19:59:04 -0600

    Added assertions for the `{x}.RandomElement()` extension methods in addition to the `Random.NextElement({x})` ones

--- BSharp.Core.Tests/RandomExtensionsTests.cs
+++ BSharp.Core.Tests/RandomExtensionsTests.cs
@@ -19,29 +19,32 @@ public class RandomExtensionsTests
     public void NextElement_IList()
     {
         var choices = new[] { 1, 2, 3 };
-        var viaExtension = CreateRandom().NextElement(choices);
+        var viaRandom = CreateRandom().NextElement(choices);
+        var viaCollection = choices.RandomElement(CreateRandom());
         var viaVanilla = choices[CreateRandom().Next(choices.Length)];
 
-        Assert.That(viaExtension, Is.EqualTo(viaVanilla));
+        Assert.That(new[] { viaRandom, viaCollection }, Has.All.EqualTo(viaVanilla));
     }
 
     [Test]
     public void NextElement_Span()
     {
         Span<int> choices = new[] { 1, 2, 3 };
-        var viaExtension = CreateRandom().NextElement(choices);
+        var viaRandom = CreateRandom().NextElement(choices);
         var viaVanilla = choices[CreateRandom().Next(choices.Length)];
+        var viaCollection = choices.RandomElement(CreateRandom());
 
-        Assert.That(viaExtension, Is.EqualTo(viaVanilla));
+        Assert.That(new[] { viaRandom, viaCollection }, Has.All.EqualTo(viaVanilla));
     }
 
     [Test]
     public void NextElement_RoSpan()
     {
         ReadOnlySpan<int> choices = new[] { 1, 2, 3 };
-        var viaExtension = CreateRandom().NextElement(choices);
+        var viaRandom = CreateRandom().NextElement(choices);
+        var viaCollection = choices.RandomElement(CreateRandom());
         var viaVanilla = choices[CreateRandom().Next(choices.Length)];
 
-        Assert.That(viaExtension, Is.EqualTo(viaVanilla));
+        Assert.That(new[] { viaRandom, viaCollection }, Has.All.EqualTo(viaVanilla));
     }
 }
