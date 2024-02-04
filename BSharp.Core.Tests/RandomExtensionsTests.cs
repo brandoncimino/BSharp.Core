fd338b0ec1e7671839333099fb812b5f8eaebf7d
Brandon Cimino <brandon.cimino@gmail.com>
2024-02-03 19:36:17 -0600

    - Added `IndexRange` and `IsExternalInit` dependencies - Updated (most) dependencies - Updated tests to ALSO target `netcoreapp3.1` (to simulate the `netstandard2.1` target)

--- BSharp.Core.Tests/RandomExtensionsTests.cs
+++ BSharp.Core.Tests/RandomExtensionsTests.cs
@@ -7,10 +7,14 @@ public class RandomExtensionsTests
 {
     private static Random CreateRandom([CallerMemberName] string? caller = null)
     {
-        ArgumentNullException.ThrowIfNull(caller);
+        if (caller == null)
+        {
+            throw new ArgumentNullException(nameof(caller), "how?!");
+        }
+
         return new Random(caller.Sum(static c => c));
     }
-    
+
     [Test]
     public void NextElement_IList()
     {
@@ -37,7 +41,7 @@ public class RandomExtensionsTests
         ReadOnlySpan<int> choices = new[] { 1, 2, 3 };
         var viaExtension = CreateRandom().NextElement(choices);
         var viaVanilla = choices[CreateRandom().Next(choices.Length)];
-        
+
         Assert.That(viaExtension, Is.EqualTo(viaVanilla));
     }
 }
